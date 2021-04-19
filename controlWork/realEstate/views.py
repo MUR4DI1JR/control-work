8ee914e559dc3d5a4cc3c4a8ab20480583cd636c
Muradil <04072002mu@gmail.com>
2021-04-19 14:50:29 +0600

    added search

--- controlWork/realEstate/views.py
+++ controlWork/realEstate/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import RealEstate
 
 def real_estate(request):
 	real_Estate = RealEstate.objects.all()
-	return render(request, 'realestate/listRealEstate.html', {'real_estate': real_Estate})
+	search_estate = request.GET.get('search_estate', '')
 
+	if search:
+		real_Estate = RealEstate.objects.filter(Q(name = search_estate) | Q(type = search_estate))
+	else:
+		real_Estate = RealEstate.objects.all()
+
+	return render(request, 'realestate/listRealEstate.html', {'real_estate': real_Estate, 'search_estate': search_estate})
